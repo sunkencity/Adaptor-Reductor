e775a1f5ddb48fdd219f51fbc9d995b10bb21f7f
sunkencity <tux@well.com>
2010-08-24 13:12:55 +0200

    working parallelization and printing

--- src/adaptor_reductor/core.clj
+++ src/adaptor_reductor/core.clj
@@ -7,38 +7,48 @@
   [file-path] (partition 4 (re-split #"\n" (slurp file-path))))
 
 (defn sequence-without-adapter
-	[sequence adapter min-length]
-	(apply str
-		(loop [s sequence
-			     a adapter
-			     match-size 0]
-			     (if (= (first s) (first a))
-				      (recur (rest s) (rest a) (+ 1 match-size))
-		          (if (>= min-length match-size)
-			          s
-			          sequence)))))
+  [sequence adapter min-length]
+  (apply str
+    (loop [s sequence
+           a adapter
+           match-size 0]
+           (if (= (first s) (first a))
+              (recur (rest s) (rest a) (+ 1 match-size))
+              (if (>= min-length match-size)
+                s
+                sequence)))))
 
 
 (defn remove-adapter-dna
-	[sequence]
+  [sequence]
   (sequence-without-adapter sequence "CTGTAGGCACCATCAATCGTATGCCGTCTTCTGCTTG" 6 ))
-	
+
 (defn with-parsed-data
   "Appends parsed data to each item"
   [data]
-  (list data (remove-adapter-dna (second data))))
+  (cons
+   (remove-adapter-dna (second data))
+   data))
 
 (defn parse-adapter
-  [file-path] (map with-parsed-data (raw-data file-path)))
+  [file-path]
+  (map with-parsed-data (raw-data file-path)))
 
 (defn parse-adapter-parallel
-  [file-path] (pmap with-parsed-data (raw-data file-path)))
+  [file-path]
+  (pmap with-parsed-data (raw-data file-path)))
+
+(defn print-items
+  [coll]
+  (doseq [x coll]
+    (doseq [line x] (println line))))
 
 (defn -main
-	"usage: datadir/trimtest.fastq -p"
-	([file] (-main file false))
-	([file parallel]
-	(apply println
-		(if (= parallel "-p")
-	  	(parse-adapter-parallel file)
-	  	(parse-adapter file )))))
+  "usage: datadir/trimtest.fastq -p"
+  ([file] (-main file false))
+  ([file parallel]
+    (if (= parallel "-p")
+      (do
+        (print-items (parse-adapter-parallel file))
+        (shutdown-agents))
+      (print-items (parse-adapter file)))))
