2f355f0c994393d9cf0c4718315c187389b8f1ed
sunkencity <tux@well.com>
2010-08-26 11:09:03 +0200

    added lein run

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[leiningen-run "0.3"]]
   :main adaptor-reductor.core)
