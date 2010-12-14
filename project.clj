f075038a8bb30a73d5d582ef0bce48f5ce32c662
sunkencity <tux@well.com>
2010-12-14 20:23:27 +0100

    added big test file

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [
-                     [leiningen-run "0.3"]
                      [swank-clojure "1.3.0-SNAPSHOT"]]
   :main adaptor-reductor.core)
