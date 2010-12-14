3e9e0ddd570a85bdb425d757c5ba0d2f742489b1
sunkencity <tux@well.com>
2010-12-14 19:26:33 +0100

    added swank-clojure

--- project.clj
+++ project.clj
@@ -2,5 +2,7 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[leiningen-run "0.3"]]
+  :dev-dependencies [
+                     [leiningen-run "0.3"]
+                     [swank-clojure "1.3.0-SNAPSHOT"]]
   :main adaptor-reductor.core)
